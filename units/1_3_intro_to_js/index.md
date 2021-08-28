ad9437be485818cb461bd10cbd9b5c17a4821e6b
Lester Lee <contact@lester-lee.com>
2021-08-28 13:13:18 -0400

    Start working on tutorial 1.3; need to style

---
+++ units/1_3_intro_to_js/index.md
@@ -0,0 +1,16 @@
+---
+layout: base
+---
+# Tutorial 1.3: Introduction to JS
+[Github link](https://github.com/lester-lee/Interactive-Data-Vis-Fall2021/tree/main/units/1_3_intro_to_js)
+
+The goal of this tutorial is to introduce interactivity by using JavaScript.
+
+---
+
+<input id="Name-input" placeholder="Name">
+<button id="Counter-input">Click here!</button>
+
+<span id="Name-text">???</span> has clicked the button <span id="Counter-text">0</span> times!
+
+<script src="./main.js"/>
--- units/1_3_intro_to_js/main.js
+++ units/1_3_intro_to_js/main.js
@@ -1,1 +1,24 @@
-console.log('hello world');
+const $name_input = document.getElementById("Name-input");
+const $counter_input = document.getElementById("Counter-input");
+const $name_text = document.getElementById("Name-text");
+const $counter_text = document.getElementById("Counter-text");
+
+/**
+ * Update the name whenever
+ * user types in the text box
+ */
+function updateName() {
+  $name_text.innerText = $name_input.value;
+}
+$name_input.oninput = updateName;
+
+/**
+ * Increment the counter by 1 whenever
+ * user clicks the button.
+ */
+let count = 0;
+function updateCount() {
+  count++;
+  $counter_text.innerText = count;
+}
+$counter_input.onclick = updateCount;
