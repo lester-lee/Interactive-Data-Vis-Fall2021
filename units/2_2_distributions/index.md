3b4265f6797c75c9a17d35359452682876d27112
Lester Lee <contact@lester-lee.com>
2021-10-09 16:59:16 -0400

    Finish 2.2 scatterplot

--- units/2_2_distributions/index.md
+++ units/2_2_distributions/index.md
@@ -1,13 +1,18 @@
 ---
 layout: base
+includeStyle: true
 ---
 # Section 2.2 | Distributions
 <figure id="HeartViz">
 <figcaption>
-Scatterplot with resting blood pressure on the X axis against cholesterol on the Y axis.
+Scatterplot with resting blood pressure on the X axis against cholesterol on the Y axis. Red color indicates heart disease. Bigger circles correspond to greater maximum heart rates acheived.
 </figcaption>
 </figure>
 
-I used a [heart failure prediction dataset from Kaggle](https://www.kaggle.com/fedesoriano/heart-failure-prediction) for this assignment.
+I used a [heart failure prediction dataset from Kaggle](https://www.kaggle.com/fedesoriano/heart-failure-prediction) for this assignment. Getting the points on the page was straightforward, as well as adding more scales to adjust the color and the size of each circle. 
+
+I again had a lot of difficulty with positioning the text, especially the rotated Y-axis label. I definitely have a better understanding of transform and rotate now, but I feel like there's a better way to do it than what I currently have.
+
+I used `css` to make the labels appear on hover, but it's not ideal right now because they appear under other data points and can be hard to read. Hopefully as I continue to work with d3 I figure out better ways of showing data points!
 
 <script src="main.js"></script>
--- units/2_2_distributions/main.js
+++ units/2_2_distributions/main.js
@@ -1,18 +1,117 @@
-/* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+d3.csv("../../data/heart.csv", d3.autoType).then((data) => {
+  //-----------------------------
+  //#region Data Preprocessing
+  //-----------------------------
+  // Ensure relevant measures are above 0
+  data = data.filter((d) => d.RestingBP > 0 && d.Cholesterol > 0);
+  //#endregion ------------------
 
-/* LOAD DATA */
-d3.csv("../../data/heart.csv", d3.autoType)
-  .then(data => {
-    console.log('data :>> ', data);
+  //-----------------------------
+  //#region Container and Constants
+  //-----------------------------
+  const container = d3
+    .select("#HeartViz")
+    .insert("svg", ":first-child")
+    .attr("height", "400");
 
-    const container = d3.select("#HeartViz")
-      .insert("svg", ":first-child")
+  const [WIDTH, HEIGHT] = getDimensions(container);
+  const MARGIN = 50; // TODO: change later
+  //#endregion ------------------
 
-    const WIDTH, HEIGHT = getDimensions(container);
-    
+  //-----------------------------
+  //#region Scales
+  //-----------------------------
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, (d) => d.RestingBP))
+    .range([MARGIN, WIDTH - MARGIN])
+    .nice();
 
-  });
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, (d) => d.Cholesterol))
+    .range([HEIGHT - MARGIN, MARGIN])
+    .nice();
+
+  const sizeScale = d3
+    .scaleSqrt()
+    .domain(d3.extent(data, (d) => d.MaxHR))
+    .range([1, 5]);
+
+  const colorScale = d3
+    .scaleOrdinal()
+    .domain(["0", "1"])
+    .range(["#bbb8", "#be2e2e"]);
+  //#endregion ------------------
+
+  //-----------------------------
+  //#region Rendering
+  //-----------------------------
+  const points = container
+    .append("g")
+    .attr("class", ".Points")
+    .selectAll(".Point")
+    .data(data)
+    .join("g")
+    .attr(
+      "transform",
+      (d) => `translate(${xScale(d.RestingBP)}, ${yScale(d.Cholesterol)})`
+    );
+
+  points
+    .attr("class", "Point")
+    .append("circle")
+    .attr("r", (d) => sizeScale(d.MaxHR))
+    .style("fill", (d) => colorScale(d.HeartDisease));
+
+  points
+    .append("text")
+    .text((d) => `(${d.RestingBP}, ${d.Cholesterol})`)
+    .attr("class", "label");
+
+  //#endregion ------------------
+
+  //-----------------------------
+  //#region Axes
+  //-----------------------------
+
+  //#endregion ------------------
+
+  // X Axis
+  container
+    .append("g")
+    .attr("class", "Axis --x")
+    .style("transform", `translate(0, ${HEIGHT - MARGIN}px)`)
+    .call(d3.axisBottom(xScale))
+    .append("text")
+    .text("Resting Blood Pressure")
+    .attr("fill", "#333")
+    .attr("x", `${WIDTH / 2}`)
+    .attr("font-size", "14")
+    .attr("y", "30");
+
+  // Y Axis
+  container
+    .append("g")
+    .attr("class", "Axis --y")
+    .style("transform", `translate(${MARGIN}px, 0px)`)
+    .call(d3.axisLeft(yScale))
+    .append("text")
+    .attr("transform", `rotate(270, 0, ${HEIGHT / 2}) translate(0, 165) `)
+    .text("Cholesterol")
+    .attr("text-anchor", "middle")
+    .attr("fill", "#333")
+    .attr("font-size", "14");
+
+  //#endregion ------------------
+
+  /*
+  const xHist = d3.histogram()
+    .value(d => d.RestingBP)
+    .domain(xScale.domain())
+    .thresholds(xScale.ticks(25))
+
+  const xBins = xHist(data)
+  console.log('xBins :>> ', xBins);
+  */
+});
